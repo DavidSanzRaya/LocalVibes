0493667af4b6fb170b025ba426408b609c8711ad
David <dsanzraya@hotmail.com>
2024-11-29 13:54:57 +0100

    login and user signup viewmodels

--- LocalVibes/Models/ViewModels/LoginViewModel.cs
+++ LocalVibes/Models/ViewModels/LoginViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace LocalVibes.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalVibes.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email adress")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Band account")]
+        public bool IsABand { get; set; }
     }
 }
