dc41e45be9b9e55530df677273fb80dd52be781b
David <dsanzraya@hotmail.com>
2024-11-29 11:17:27 +0100

    user, project, member models

---
+++ LocalVibes/Models/Member.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalVibes.Models
+{
+    public class Member
+    {
+        public int IdMember { get; set; }
+        public string MemberName { get; set; }
+        public string? ArtisticName { get; set; }
+        public byte[]? MemberImage { get; set; }
+        public int FKRolInstrument { get; set; }
+    }
+}
---
+++ LocalVibes/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace LocalVibes.Models
+{
+    public class Project
+    {
+        public int IdProject { get; set; }
+        public string ProjectName { get; set; }
+        public string? Biography { get; set; }
+        public DateTime? FormationDate { get; set; }
+        public byte[]? ProjectImage { get; set; }
+        public int FKUser { get; set; }
+    }
+}
