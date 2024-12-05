a78b42d358bb5447b185af981c8b1262bdc42411
Abel Cumbreño Pinadero <abelcumbrenobalmes@gmail.com>
2024-12-05 09:48:23 +0100

    Se ha añadido KEY en modelos

--- LocalVibes/Models/UserFavoriteProject.cs
+++ LocalVibes/Models/UserFavoriteProject.cs
@@ -5,7 +5,7 @@ namespace LocalVibes.Models
     // Tabla UserFavoriteProject que conecta Users con Project
     public class UserFavoriteProject
     {
-        public int idUser {  get; set; } // FK de Users
-        public int idProject { get; set; } // FK de Project
+        public int IdUsers {  get; set; } // FK de Users
+        public int IdProject { get; set; } // FK de Project
     }
 }
