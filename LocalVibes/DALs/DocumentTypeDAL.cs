eda229849d9812c33856df82fa11fc1c2707b44c
Abel Cumbreño Pinadero <abelcumbrenobalmes@gmail.com>
2024-12-06 18:12:08 +0100

    Cambios visuales y de logica tanto de la pagina login/signUp como de Landing entre otras

---
+++ LocalVibes/DALs/DocumentTypeDAL.cs
@@ -0,0 +1,22 @@
+﻿using LocalVibes.Models;
+using LocalVibes.Tools;
+using Microsoft.Data.SqlClient;
+
+namespace LocalVibes.DALs
+{
+    public class DocumentTypeDAL : DAL<DocumentType>
+    {
+        protected override string TableName => "DocumentType";
+
+
+        protected override DocumentType MapReaderToEntity(SqlDataReader reader)
+        {
+            return new DocumentType
+            {
+                IdDocumentType = (int)reader["IdDocumentType"],
+                NameDocumentType = (string)reader["NameDocumentType"],
+            };
+        }
+
+    }
+}
