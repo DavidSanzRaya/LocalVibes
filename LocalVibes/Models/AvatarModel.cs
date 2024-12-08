80a9782f4520624f768ed9cf95036f11314fe89c
joelnexia <joelforworks@gmail.com>
2024-12-08 17:52:42 +0100

    feat: Revieew card

--- LocalVibes/Models/AvatarModel.cs
+++ LocalVibes/Models/AvatarModel.cs
@@ -3,7 +3,7 @@ namespace LocalVibes.Models
     public class AvatarModel
     {
         public string ImageUrl { get; set; } = "https://pbs.twimg.com/media/Gd_LKDxWgAAuH5p?format=jpg&name=medium";
-        
+        public string Size { get; set; } = "100px";
     }
 }
 
